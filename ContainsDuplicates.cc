d601a9174b73dc685745732f9e0566286c58a83b
Chantelle <ccabanilla@unr.edu>
2024-09-29 13:48:01 -0700

    idk how this works

---
+++ ContainsDuplicates.cc
@@ -0,0 +1,24 @@
+#include <unordered_set>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+      bool containsDuplicate(vector<int>& nums) {
+          
+          unordered_set<int> crossoff;
+          for(int nums: x){
+              crossoff.count(x){
+                  return true;
+              }
+              crossoff.insert(x);
+              return false;
+          }
+      }
+  };
+  
+  int main(){
+      containsDuplicate();
+  }
