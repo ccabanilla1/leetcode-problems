901ba7bc26d4e160b58d53a941d515c717ee049d
Chantelle <ccabanilla@unr.edu>
2024-11-12 10:07:54 -0800

    containsduplicates

--- ContainsDuplicates.cc
+++ ContainsDuplicates.cc
@@ -6,19 +6,34 @@ using namespace std;
 
 class Solution {
   public:
-      bool containsDuplicate(vector<int>& nums) {
-          
+      bool containsDuplicate(vector<int>& nums)
+    {
           unordered_set<int> crossoff;
-          for(int nums: x){
-              crossoff.count(x){
+          for(int x: nums)
+          {
+              if (crossoff.count(x))
+              {
                   return true;
               }
               crossoff.insert(x);
-              return false;
           }
+              return false;
       }
   };
-  
-  int main(){
-      containsDuplicate();
-  }
+
+  int main()
+{
+  Solution solution;
+  vector<int> nums1 = {1, 2, 3, 4};
+  cout << (solution.containsDuplicate(nums1) ? "true" : "false") << endl;
+
+  vector<int> nums2 = {1, 2, 3, 1};
+  cout << (solution.containsDuplicate(nums2) ? "true" : "false") << endl;
+
+  vector<int> nums3 = {};
+  cout << (solution.containsDuplicate(nums3) ? "true" : "false") << endl;
+
+  vector<int> nums4 = {1, 1, 1, 1};
+  cout << (solution.containsDuplicate(nums4) ? "true" : "false") << endl;
+  return 0;
+}
